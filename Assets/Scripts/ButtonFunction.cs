9bf9ed68fcb1c1fe6247aa2f41c62fffcee20fd6
NK.Shien <shien091091@gmail.com>
2021-03-03 23:27:42 +0800

    新功能

---
+++ Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunction : MonoBehaviour
+{
+    public void BTN_FuntionA()
+    {
+        Debug.Log("BTN_FuntionA");
+    }
+}
